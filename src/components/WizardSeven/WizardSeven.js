d3f307f31ea84c2701a5bd6d7a313a0e0af7d0ec
Mason Galland <masongalland@me.com>
2017-07-10 16:13:43 -0600

    done

--- src/components/WizardSeven/WizardSeven.js
+++ src/components/WizardSeven/WizardSeven.js
@@ -1,5 +1,7 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { updateCredit } from '../../ducks/reducer'
+import { connect } from 'react-redux'; 
 
 class WizardSeven extends Component {
 
@@ -11,15 +13,20 @@ class WizardSeven extends Component {
                     <p>Estimate your credit score</p> <br />
                     
                     <div className="row">
-                        <Link to="/wEight"><button onClick={this.props.updateCreditE}>Excellent</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditG}>Good</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditF}>Fair</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditP}>Poor</button></Link>
+                        <Link to="/wEight"><button onClick={ (e)=>this.props.updateCredit('Excellent') }>Excellent</button></Link>
+                        <Link to="/wEight"><button onClick={ (e)=>this.props.updateCredit('Good') }>Good</button></Link>
+                        <Link to="/wEight"><button onClick={ (e)=>this.props.updateCredit('Fair') }>Fair</button></Link>
+                        <Link to="/wEight"><button onClick={ (e)=>this.props.updateCredit('Poor') }>Poor</button></Link>
                     </div>
                 </div>
             </div>
         )
     }
 }
+function mapStateToProps( state ) {
+  return { 
+      credit: state.credit,
+    };
+}
 
-export default WizardSeven;
+export default connect(mapStateToProps, { updateCredit })(WizardSeven);
