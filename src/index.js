b0e26eb4d05f5f803bbc6b6578a7df475d856ae0
Mason Galland <masongalland@me.com>
2017-07-10 14:32:21 -0600

    finished through step 3

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import {Provider} from 'react-redux';
+import store from './store'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
 
